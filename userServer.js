64348028aeb897ef9e3ca949b4d8c2342763187a
ShaQay3Q <shaghay3gh@gmail.com>
2024-11-27 13:48:03 +0100

    use bcrypt to hash the pass

--- userServer.js
+++ userServer.js
@@ -1,6 +1,10 @@
 // imports the Express library
 const express = require("express");
 
+// For hashing
+// const argon2 = require("argon2");
+const bcrypt = require("bcrypt");
+
 // initializes a new Express application instance
 const app = express();
 
@@ -15,10 +19,24 @@ app.get("/users", (req, res) => {
 	res.json(users);
 });
 
-app.post("/users", (req, res) => {
-	const user = { name: req.body.name, password: req.body.password };
-	users.push(user);
-	res.status(201).send("Successful");
+app.post("/users", async (req, res) => {
+	try {
+		// const newSalt = await argon2.genSalt(10);
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+		console.log(salt);
+		console.log(hashedPassword);
+
+		const user = { name: req.body.name, password: hashedPassword };
+		users.push(user);
+		res.status(201).send("Successful");
+		// hash("ThisIsAPassword");
+	} catch (error) {
+		console.log(error);
+
+		res.status(500).send("Internal Server Error");
+	}
 });
 
-app.listen(5000)
+app.listen(5000);
