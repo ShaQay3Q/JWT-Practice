7cf409072faf092eb79bb96a06fe6d2ff4b5962d
ShaQay3Q <shaghay3gh@gmail.com>
2024-11-28 21:44:33 +0100

    applying css on ejs files

---
+++ serverStart.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+app.set("view-engine", "ejs");
+app.use(express.static("public")); //better and newer way of applying css
+
+app.get("/", (req, res) => {
+	// send a certain page
+	res.render("index.ejs", { name: "Sha" });
+});
+app.get("/login", (req, res) => {
+	res.render("login.ejs");
+});
+app.get("/register", (req, res) => {
+	res.render("register.ejs");
+});
+
+app.listen(3001);
