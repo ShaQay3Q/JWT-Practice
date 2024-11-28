c17705e2bc180c8e9b80f3368a70008f5e45b2ed
ShaQay3Q <shaghay3gh@gmail.com>
2024-11-28 22:46:16 +0100

    can register a user

--- serverStart.js
+++ serverStart.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 app.set("view-engine", "ejs");
 app.use(express.static("public")); //better and newer way of applying css
+app.use(express.urlencoded({ extended: false }));
+
+const users = [];
 
 app.get("/", (req, res) => {
 	// send a certain page
@@ -10,6 +14,27 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
 	res.render("login.ejs");
 });
+
+app.post("/register", async (req, res) => {
+	try {
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(req.body.password, salt);
+		const user = {
+			id: Date.now().toString(),
+			name: req.body.name,
+			email: req.body.email,
+			password: hashedPassword,
+		};
+		users.push(user);
+		// res.status(201).send("Successful");
+		res.redirect("/login");
+	} catch {
+		// res.status(500).send("Internal Server Error");
+		res.redirect("/register");
+	}
+	console.log(users);
+});
+
 app.get("/register", (req, res) => {
 	res.render("register.ejs");
 });
